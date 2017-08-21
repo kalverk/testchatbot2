a37a107b5114c71330e6a3a30b3cba190a60196b
Kalver Kilvits <kalver.kilvits@reach-u.com>
2017-08-21 15:34:18 +0300

    debug

--- app.py
+++ app.py
@@ -102,8 +102,9 @@ def send_message(recipient_id, message_text):
 
             response["attachment"]["payload"]["elements"].append(template);
 
-
+    log("RESPONSE")
     log(response)
+    log("END RESPONSE")
 
     log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
 
@@ -113,15 +114,24 @@ def send_message(recipient_id, message_text):
     headers = {
         "Content-Type": "application/json"
     }
+
     data = json.dumps({
         "recipient": {
             "id": recipient_id
         },
-        # "message": {
-        #     "text": message_text
-        # },
-        "message": response
+        "message": {
+            "text": "Could not find the requested city"
+        }
     })
+
+    if response["attachment"]["payload"]["elements"].length > 0:
+        data = json.dumps({
+            "recipient": {
+                "id": recipient_id
+            },
+            "message": response
+        })
+
     r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
     if r.status_code != 200:
         log(r.status_code)
